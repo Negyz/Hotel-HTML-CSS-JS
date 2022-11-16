50b727c65691697a9a835301d54e4615e9a1a6c2
ìtalo Brandão <italobrandao5438@gmail.com>
2022-11-15 23:17:39 -0300

    At3

---
+++ js/js.js
@@ -0,0 +1,11 @@
+<form>
+
+        <input type="text" name="email" placeholder="Seu e-mail:"/>
+        <br>
+        <input type="text" name="Nome" placeholder="Seu nome:"/>
+    
+    <br>
+        <textarea name="mensagem" rows="10" cols="30" ></textarea>
+    <br> <input onclick="alert('Observação encaminhada')" type="Submit" name="acao" value="Enviar"/>
+    </form>                                  
+    </body>
