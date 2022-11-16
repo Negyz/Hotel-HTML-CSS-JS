e5237dcbdcf2749910c45e6cc1c856f6a443c166
ìtalo Brandão <italobrandao5438@gmail.com>
2022-11-15 23:19:35 -0300

    AT3

--- js/js.js
+++ js/js.js
@@ -1,11 +1,12 @@
-<form>
-
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Java Script - Hotel</title>
+    </head>
+    <body>
         <input type="text" name="email" placeholder="Seu e-mail:"/>
-        <br>
-        <input type="text" name="Nome" placeholder="Seu nome:"/>
-    
-    <br>
+        <input type="text" name="Nome" placeholder="Seu nome:"/>   
         <textarea name="mensagem" rows="10" cols="30" ></textarea>
-    <br> <input onclick="alert('Observação encaminhada')" type="Submit" name="acao" value="Enviar"/>
-    </form>                                  
-    </body>
+        <input onclick="alert('Observação encaminhada')" type="Submit" name="acao" value="Enviar"/>
+</body>
+</html>
